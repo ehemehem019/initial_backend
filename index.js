d5f0c9cd48f9fb49c9b9ff041271f1df43060aae
Nehemiah Ellorico <ellorico.nj@gmail.com>
2023-06-12 23:24:07 +0800

    initial backend

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3001;
+
+mongoose.connect(
+  "mongodb+srv://admin123:admin123@zuitt-bootcamp.cnhvgta.mongodb.net/nestle_stock_historical_price?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+let db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => console.log("We're connected to the cloud database"));
+
+// Schema
+const stockPriceSchema = new mongoose.Schema({
+  Date: String,
+  OpenPrice: Number,
+  HighPrice: Number,
+  LowPrice: Number,
+  ClosePrice: Number,
+  WAP: Number,
+  "No.ofShares": Number,
+  "No.ofTrades": Number,
+  "TotalTurnover(Rs.)": Number,
+  DeliverableQuantity: Number,
+  "%Deli.QtytoTradedQty": Number,
+  SpreadHighLow: Number,
+  SpreadCloseOpen: Number,
+});
+
+// Model
+const StockPrice = mongoose.model(
+  "StockPrice",
+  stockPriceSchema,
+  "nestle_stock_prices"
+);
+
+app.get("/stockprices", (req, res) => {
+  StockPrice.find({})
+    .exec()
+    .then((result) => {
+      return res.status(200).json({
+        data: result,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    });
+});
+
+app.listen(port, () => console.log(`Server running at port ${port}`));
